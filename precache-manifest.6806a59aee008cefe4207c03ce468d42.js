a27f4f8f525732a0b1e1fe7ec38347d4678d64b0
Nora Mate <norama.matema@gmail.com>
2019-02-04 00:34:33 +0100

    Updates

---
+++ precache-manifest.6806a59aee008cefe4207c03ce468d42.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "ea5838d797847e96083e",
+    "url": "/to-do-hooks/static/js/runtime~main.ea5838d7.js"
+  },
+  {
+    "revision": "0ad7c77cdabe642d303a",
+    "url": "/to-do-hooks/static/js/main.0ad7c77c.chunk.js"
+  },
+  {
+    "revision": "4c2ef9b754da75deb71a",
+    "url": "/to-do-hooks/static/js/1.4c2ef9b7.chunk.js"
+  },
+  {
+    "revision": "4c2ef9b754da75deb71a",
+    "url": "/to-do-hooks/static/css/1.e986687e.chunk.css"
+  },
+  {
+    "revision": "d9fce85f4c1404bf8eb5435af1ac9a75",
+    "url": "/to-do-hooks/index.html"
+  }
+];
